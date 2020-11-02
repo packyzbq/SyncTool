6c8f50f1c9be16667e8763245ccaa710013afc66
packyzbq <packyzhao@163.com>
2020-11-02 13:41:11 +0800

    fix sync script

--- image-sync/sync.sh
+++ image-sync/sync.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-TARGET_REPO=$TARGET_REPO:-
+TARGET_REPO=${TARGET_REPO:-"registry.cn-hangzhou.aliyuncs.com/packyzbq/public"}
 for image in `cat images_list`
 do
     echo " start sync image $line"
     docker pull $line
-    docker tag $line registry.cn-hangzhou.aliyuncs.com/packyzbq/public:${line/:/-}
-    docker push registry.cn-hangzhou.aliyuncs.com/packyzbq/public:${line/:/-}
+    docker tag $line ${TARGET_REPO}:${line/:/-}
+    docker push ${TARGET_REPO}:${line/:/-}
 done
