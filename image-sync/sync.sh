718716988dfddebe54daa2dd3ae3244179631847
packyZhao <packyzhao@163.com>
2020-11-02 14:00:05 +0800

    Update sync.sh

--- image-sync/sync.sh
+++ image-sync/sync.sh
@@ -4,7 +4,7 @@ for line in `cat image-sync/images_list`
 do
     echo " start sync image $line"
     docker pull $line
-    docker login -u $ACS_USERNAME -p $ACS_PASSWORD
+    docker login -u $ACS_USERNAME -p $ACS_PASSWORD registry.cn-hangzhou.aliyuncs.com
     docker tag $line ${TARGET_REPO}:${line/:/-}
     docker push ${TARGET_REPO}:${line/:/-}
 done
