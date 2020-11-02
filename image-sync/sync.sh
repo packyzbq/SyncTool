a4735fcbff7ed9ff1fea612088ff023554c3e4b9
packyzbq <packyzhao@163.com>
2020-11-02 13:56:25 +0800

    ADD: acs user&passwd

--- image-sync/sync.sh
+++ image-sync/sync.sh
@@ -4,6 +4,7 @@ for line in `cat image-sync/images_list`
 do
     echo " start sync image $line"
     docker pull $line
+    docker login -u $ACS_USERNAME -p $ACS_PASSWORD
     docker tag $line ${TARGET_REPO}:${line/:/-}
     docker push ${TARGET_REPO}:${line/:/-}
 done
