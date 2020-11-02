232180d642534da73900d5b182c9c302a18886fb
packyzbq <packyzhao@163.com>
2020-11-02 14:19:52 +0800

    update images

--- image-sync/sync.sh
+++ image-sync/sync.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-TARGET_REPO=${TARGET_REPO:-"registry.cn-hangzhou.aliyuncs.com/packyzbq/public"}
+TARGET_REPO=${TARGET_REPO:-"registry.cn-hangzhou.aliyuncs.com"}
+NAMESPACE=${NAMESPACE:-"packyzbq"}
 for line in `cat image-sync/images_list`
 do
     echo " start sync image $line"
     docker pull $line
     docker login -u $ACS_USERNAME -p $ACS_PASSWORD registry.cn-hangzhou.aliyuncs.com
-    docker tag $line ${TARGET_REPO}:${line/:/-}
-    docker push ${TARGET_REPO}:${line/:/-}
+    docker tag $line ${TARGET_REPO}/${NAMESPACE}/$line
+    docker push ${TARGET_REPO}/${NAMESPACE}/$line
 done
