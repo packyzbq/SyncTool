65c6b1d6de3143db1b89e217a410fee47bbea451
packyzbq <packyzhao@163.com>
2020-11-17 14:15:49 +0800

    add arch support

--- image-sync/sync.sh
+++ image-sync/sync.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 TARGET_REPO=${TARGET_REPO:-"registry.cn-hangzhou.aliyuncs.com"}
 NAMESPACE=${NAMESPACE:-"packyzbq"}
+extra_args=
 for line in `cat image-sync/images_list`
 do
-    echo " start sync image $line"
-    docker pull $line
-    docker login -u $ACS_USERNAME -p $ACS_PASSWORD registry.cn-hangzhou.aliyuncs.com
-    docker tag $line ${TARGET_REPO}/${NAMESPACE}/$line
-    docker push ${TARGET_REPO}/${NAMESPACE}/$line
+    if [[ ${line:0:1} != "#" ]]
+    then
+        arr=(${line//@/ })
+        image=${arr[0]}
+        if [[ ${#arr[@]} -eq 2 ]]
+        then
+            extra_args="$extra_args --platform ${arr[1]}"
+        fi
+        echo " start sync image $image"
+        docker pull $line
+        docker login -u $ACS_USERNAME -p $ACS_PASSWORD registry.cn-hangzhou.aliyuncs.com
+        docker tag $image ${TARGET_REPO}/${NAMESPACE}/$image
+        docker push ${TARGET_REPO}/${NAMESPACE}/$image
+    fi
 done
